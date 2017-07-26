b8ed8cb339dc3ef1ff88b19d275c4f14a34612b3
goatpig <moothecowlord@gmail.com>
2017-07-26 03:24:16 +0200

    fix description

--- armoryd.py
+++ armoryd.py
@@ -2106,7 +2106,7 @@ class Armory_Json_Rpc_Server(jsonrpc.JSONRPC):
       to 0
       PARAMETERS:
       txASCII - A transaction in ASCII-armored form.
-      Message to add to the op_return output. Can be empty
+      message - Message to add to the op_return output. Can be empty
       RETURN:
       A dict with these 2 possible keys:
          Tx: a transaction in ASCII-armored form.
